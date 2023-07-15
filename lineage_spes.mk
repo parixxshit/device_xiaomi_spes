2f0b5d4c7b4b31bca1e6866d9ff87b3d111608c9
Michael Bestas <mkbestas@lineageos.org>
2023-07-15 18:22:45 +0530

    spes: Opt out of EPPE
    Change-Id: I71f54b1ad6de9d1406a3a55239acff1f4e426158
    Signed-off-by: Parixshit <87859520+Parixshit@users.noreply.github.com>

--- lineage_spes.mk
+++ lineage_spes.mk
@@ -10,6 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+TARGET_DISABLE_EPPE := true
 
 # Inherit from spes device
 $(call inherit-product, device/xiaomi/spes/device.mk)
